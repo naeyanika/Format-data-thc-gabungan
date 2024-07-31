af8f758e433fd2baae724b141276c4242dc9d4b7
naeyanika <40598758+naeyanika@users.noreply.github.com>
2024-07-31 11:25:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,36 @@ if uploaded_files:
             if col not in df1.columns:
                 df1[col] = 0
 
-        
-            st.write("TAK TOTAL:")
-            st.write(df1)
+        rename_dict = {
+                'KELOMPOK': 'KEL',
+                'DEBIT_PINJAMAN ARTA': 'Db PRT',
+                'DEBIT_PINJAMAN DT. PENDIDIKAN': 'Db DTP',
+                'DEBIT_PINJAMAN MIKROBISNIS': 'Db PMB',
+                'DEBIT_PINJAMAN SANITASI': 'Db PSA',
+                'DEBIT_PINJAMAN UMUM': 'Db PU',
+                'DEBIT_PINJAMAN RENOVASI RUMAH': 'Db PRR',
+                'DEBIT_PINJAMAN PERTANIAN': 'Db PTN',
+                'DEBIT_TOTAL': 'Db Total2',
+                'CREDIT_PINJAMAN ARTA': 'Cr PRT',
+                'CREDIT_PINJAMAN DT. PENDIDIKAN': 'Cr DTP',
+                'CREDIT_PINJAMAN MIKROBISNIS': 'Cr PMB',
+                'CREDIT_PINJAMAN SANITASI': 'Cr PSA',
+                'CREDIT_PINJAMAN UMUM': 'Cr PU',
+                'CREDIT_PINJAMAN RENOVASI RUMAH': 'Cr PRR',
+                'CREDIT_PINJAMAN PERTANIAN': 'Cr PTN',
+                'CREDIT_TOTAL': 'Cr Total2'
+            }
+            
+            df1 = df1.rename(columns=rename_dict)
+            
+            desired_order = [
+            'ID ANGGOTA', 'DUMMY', 'NAMA', 'CENTER', 'KEL', 'HARI', 'JAM', 'SL', 'TRANS. DATE', 'Db Qurban', 'Cr Qurban', 'Db Khusus', 'Cr Khusus', 'Db Sihara', 'Cr Sihara', 'Db Pensiun', 'Cr Pensiun', 'Db Pokok', 'Cr Pokok',
+            'Db SIPADAN', 'Cr SIPADAN', 'Db Sukarela', 'Cr Sukarela', 'Db Wajib', 'Cr Wajib', 'Db Total', 'Cr Total', 'Db PTN', 'Cr PTN', 'Db PRT', 'Cr PRT', 'Db DTP', 'Cr DTP', 'Db PMB', 'Cr PMB', 'Db PRR', 'Cr PRR', 'Db PSA', 'Cr PSA', 'Db PU', 'Cr PU', 'Db Total2', 'Cr Total2'
+            ]
+
+            # Tambahkan kolom yang mungkin belum ada dalam DataFrame
+            for col in desired_order:
+                if col not in df1.columns:
+                    df1[col] = 0
+
+            df1 = df1[desired_order]
